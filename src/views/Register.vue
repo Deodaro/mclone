96c6b0276a441065d3b01d466114b51c507d094e
Deodaro <l_r@list.ru>
2021-01-27 23:13:15 +1000

    create project mclone_vue

---
+++ src/views/Register.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign Up</h1>
+          <p class="text-xs-center">
+            <!-- login - это тот name, который описан для страницы логина = алиас для страницы логина -->
+            <router-link :to="{name: 'login'}">Need an account?</router-link>
+          </p>
+          <!-- VALIDATION ERRORS -->
+          <!-- VALIDATION ERRORS END -->
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// экспортируем пустой объект
+export default {
+  // задаём внутри name - это будет название нашего компонента
+  // название лучше задавать такое, чтобы было понятно:
+  // это компонет нашего проекта или компонент сторонней библиотеки?
+  // хорошей практикой считается делать префиксы по названию проекта
+  name: 'MclRegitster',
+  methods: {
+    onSubmit() {
+      console.log('submitted form');
+    }
+  }
+};
+</script>
